94dda3cd6c42a93ff28af3f86a374b2b1ad4ed52
keisuke watanabe <kskwtnb93@gmail.com>
2024-02-12 00:06:21 +0900

    セクション２：パラメタライズドテスト（テストのループ処理）

--- src/section2/sum.test.ts
+++ src/section2/sum.test.ts
@@ -1,5 +1,13 @@
 import { sum } from "./sum";
 
-it("１と２を足すと３になる", () => {
-  expect(sum(1, 2)).toBe(3);
+// it("１と２を足すと３になる", () => {
+//   expect(sum(1, 2)).toBe(3);
+// });
+
+it.each`
+  a    | b     | expected
+  ${1} | ${2}  | ${3}
+  ${1} | ${-2} | ${-1}
+`("$aと$bを足すと$expectedになる", ({ a, b, expected }) => {
+  expect(sum(a, b)).toBe(expected);
 });
