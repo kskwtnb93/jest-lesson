0f35a0639a060e125de9c85f24a153a4fab65bae
keisuke watanabe <kskwtnb93@gmail.com>
2024-02-12 22:26:13 +0900

    メモ追加

--- src/section2/divide.test.ts
+++ src/section2/divide.test.ts
@@ -2,6 +2,8 @@
 import { divide, ZeroDivisorError } from "./divide";
 
 it("0で割るとエラーが発生する", () => {
+  // エラーのテストの場合は関数を無名関数でラップする
+  // マッチャーの.toThrow()が呼ばれる前にJestがエラーで止まってしまうため
   expect(() => divide(10, 0)).toThrow();
   expect(() => divide(10, 0)).toThrow("0で割ることはできません");
   expect(() => divide(10, 0)).toThrow(ZeroDivisorError);
